af19840b8df5a6547089d28577788725fb951ebf
harshdeep3 <harshdeep366@gmail.com>
2024-09-22 16:38:03 +0100

    updating the basic nn to not include code fo testing and updating the nn to have the correct dimensions

--- Stock_prediction/models/basic_nn_model.py
+++ Stock_prediction/models/basic_nn_model.py
@@ -9,41 +9,17 @@ class NeuralNetwork(nn.Module):
         super().__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
+            nn.Linear(7, 70),
             nn.ReLU(),
-            nn.Linear(512, 512),
+            nn.Linear(70, 512),
             nn.ReLU(),
-            nn.Linear(512, 10)
+            nn.Linear(512, 7)
         )
 
     def forward(self, x):
         x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
-
-
-def test(dataloader, model, loss_fn):
-    """ This tests performance of the model
-
-    Args:
-        dataloader (_type_): _description_
-        model (_type_): _description_
-        loss_fn (_type_): _description_
-    """
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    model.eval()
-    test_loss, correct = 0, 0
-    with torch.no_grad():
-        for X, y in dataloader:
-            X, y = X.to(device), y.to(device)
-            pred = model(X)
-            test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-    test_loss /= num_batches
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-    
 
 
 if __name__ == "__main__":

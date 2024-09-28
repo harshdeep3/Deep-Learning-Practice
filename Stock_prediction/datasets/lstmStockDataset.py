886a9cb9cd913d5846444d56a1a56ea7e56d7504
harshdeep3 <harshdeep366@gmail.com>
2024-09-28 18:57:05 +0100

    saving data to a file and adding the loading of data from the files

--- Stock_prediction/datasets/lstmStockDataset.py
+++ Stock_prediction/datasets/lstmStockDataset.py
@@ -1,11 +1,8 @@
 
-
-from torchvision import datasets
-from torchvision.transforms import ToTensor
+import torch
 
 import MetaTrader5 as mt5
-import pandas as pd
-from datetime import datetime
+import os
 import pytz
 from Stock_prediction.MT5_Link import get_historic_data
 from torch.utils.data import Dataset
@@ -13,25 +10,33 @@ from torch.utils.data import Dataset
 
 class StockDataset(Dataset):
     
-    def __init__(self, symbol:str, look_back: int, is_training: bool):
-        # metatrader info
-        # timeframe objects https://www.mql5.com/en/docs/python_metatrader5/mt5copyratesfrom_py
-        self.timeframe = mt5.TIMEFRAME_M5
-        self.symbol = symbol
-        # set time zone to UTC
-        self.timezone = pytz.timezone("Etc/UTC")
-        
-        # goes back to 1971-08-11
-        self.count = 13500
+    def __init__(self, symbol:str, look_back: int, is_training: bool, input_file: str, target_file: str):
 
-        # get data
-        df = get_historic_data(self.symbol, self.timeframe, self.count)
-        df = df.set_index('time')
-        
-        # length of dataset
-        self.training_data_len = round(len(df) * 0.8)
-        
-        self.create_input_and_target_data(df, look_back, is_training)
+        if not os.path.exists(input_file) or not os.path.exists(target_file):
+            # metatrader info
+            # timeframe objects https://www.mql5.com/en/docs/python_metatrader5/mt5copyratesfrom_py
+            self.timeframe = mt5.TIMEFRAME_M5
+            self.symbol = symbol
+            # set time zone to UTC
+            self.timezone = pytz.timezone("Etc/UTC")
+
+            # goes back to 1971-08-11
+            self.count = 13500
+
+            # get data
+            df = get_historic_data(self.symbol, self.timeframe, self.count)
+            df = df.set_index('time')
+
+            # length of dataset
+            self.training_data_len = round(len(df) * 0.8)
+
+            self.input_file = input_file
+            self.target_file = target_file
+
+            self.create_input_and_target_data(df, look_back, is_training)
+        else:
+            self.inputs = torch.load(input_file)
+            self.targets = torch.load(target_file)
 
 
     def create_input_and_target_data(self, df, look_back, is_training):
@@ -63,6 +68,9 @@ class StockDataset(Dataset):
 
             self.inputs = training_data_input
             self.targets = training_data_target
+
+            torch.save(torch.Tensor(self.inputs), self.input_file)
+            torch.save(torch.Tensor(self.targets), self.target_file)
         else:
 
             # create all possible sequences of length look_back
@@ -72,8 +80,11 @@ class StockDataset(Dataset):
                 # target data
                 testing_data_target.append(testing_data_raw[index + look_back])
 
-            self.inputs = training_data_input
-            self.targets = training_data_target
+            self.inputs = testing_data_input
+            self.targets = testing_data_target
+
+            torch.save(torch.Tensor(self.inputs), self.input_file)
+            torch.save(torch.Tensor(self.targets), self.target_file)
     
     def __len__(self):
         return len(self.inputs)

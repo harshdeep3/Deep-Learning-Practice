2dec0a260acd01bbe3d79038cf592c67ce464d91
harshdeep3 <harshdeep366@gmail.com>
2024-10-12 20:00:31 +0100

    updating the training to handle optuna study

--- Stock_prediction/training.py
+++ Stock_prediction/training.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import torch
 from torch import nn
@@ -18,19 +19,15 @@ def train_one_epoch(dataloader, model, loss_fn, optimiser, device):
         loss_fn (_type_): _description_
         optimiser (_type_): _description_
     """
+    model.to(device)
     model.train()
     
     batch_loss = []
-    batch_acc = []
     for batch, (inputs, targets) in enumerate(dataloader):
         # moving to device and changing to
         inputs, targets = inputs.type(torch.float32).to(device), targets.type(torch.float32).to(device)
-
-        inputs = torch.squeeze(inputs, 0)
-        targets = torch.squeeze(targets, 0)
-
-        pred = model(inputs)
-
+        # Compute prediction error
+        pred = torch.unsqueeze(model(inputs), 0)
         loss = loss_fn(pred, targets)
 
         # Backpropagation
@@ -39,10 +36,9 @@ def train_one_epoch(dataloader, model, loss_fn, optimiser, device):
         optimiser.zero_grad()
 
         loss, current = loss.item(), (batch + 1) * len(inputs)
-
         batch_loss.append(loss)
 
-    return np.array(batch_loss)
+    return batch_loss
 
 
 def train(epochs, dataloader, model, loss_fn, optimiser, device):
@@ -54,20 +50,15 @@ def train(epochs, dataloader, model, loss_fn, optimiser, device):
         loss_fn (_type_): _description_
         optimiser (_type_): _description_
     """
-
     epoch_loss = []
     for epoch in range(epochs):
-        # run one epoch
         loss = train_one_epoch(dataloader, model, loss_fn, optimiser, device)
-
         if epoch % 10 == 0:
             print(f"-------------------------------\nEpoch {epoch + 1}")
             print(f"Average loss: {np.average(loss)}")
-
-        epoch_loss.append(np.average(loss))
-
-    plt.plot(epoch_loss)
-    plt.show()
+            epoch_loss.append(np.average(loss))
+        plt.plot(epoch_loss)
+        plt.show()
 
 
 def save_model(model, filepath: str):
@@ -87,19 +78,22 @@ if __name__ == '__main__':
     # hyperparameters
     batch_size = 2
     lr = 1e-3
-    epochs = 200
+    epochs = 1
     input_dim = 7
-    look_back = 5
+    output_dim = 7
+    look_back = 32
+    hidden_dim = 64
+    num_layers = 1
     symbol = 'USDJPY'
     model_type = "lstm"
 
     # create model and move to device (cuda or cpu)
     # model = NeuralNetwork().to(device)
-    model = LSTM(input_dim=input_dim, output_dim=7, hidden_dim=64, num_layers=1,
+    model = LSTM(input_dim=input_dim, output_dim=output_dim, hidden_dim=hidden_dim, num_layers=num_layers,
                  device=device, batch_size=batch_size).to(device)
 
     # set loss function and optimiser
-    loss_fn = nn.MSELoss()
+    loss_fn = nn.L1Loss()
     optimiser = torch.optim.Adam(model.parameters(), lr=lr)
 
     mt5_obj = MT5Class()
@@ -113,6 +107,6 @@ if __name__ == '__main__':
     train(epochs=epochs, dataloader=train_dl, model=model, loss_fn=loss_fn, optimiser=optimiser, device=device)
 
     filepath = ("C:\\Users\\Harsh\\Desktop\\Coding Projects\\GitHub\\"
-                "Deep-Learning-Practice\\Stock_prediction\\models\\saved_models\\nn_model.pt")
+                "Deep-Learning-Practice\\Stock_prediction\\models\\saved_models\\lstm_model.pt")
 
     save_model(model, filepath=filepath)
